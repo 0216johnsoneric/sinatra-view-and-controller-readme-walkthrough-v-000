560ccfabaf360941dbc3c18037068ebe6e297840
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-07 01:52:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   post '/reversed' do
     original_string = params[:string].reverse
     @reversed_string = original_string
-
+    binding.pry
     erb :reversed
   end
 
