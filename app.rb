45bd671c4c667d0d0dc0895a12e485d35c7fd7c5
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-07 02:06:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ class App < Sinatra::Base
   end
 
   post '/reversed' do
-    
+
     original_string = params["string"]
-    @reversed_string = original_string
+    @reversed_string = original_string.reverse
 
     erb :reversed
   end
