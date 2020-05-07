5efe9a4fb43aa3f1aa81b6c9fa9022c09bf7f145
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-07 01:47:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   post '/reverse' do
-    @last_response = params[:string]
+    # @last_response = params[:string]
     erb :reverse
   end
 
